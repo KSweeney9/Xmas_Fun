52e92dc9e6b46ed31170ab406561d28b771d9723
KieranSweeney <kieransweeney52@gmail.com>
2022-12-13 16:15:32 +0000

    All presents now wrapped and some comments added

--- XmasWrap.ps1
+++ XmasWrap.ps1
@@ -1,13 +1,17 @@
+#State the gifts
 $gifts = @("dog", "socks", "coal","playstation", "Hamster")
 
-$gift = Get-Random -InputObject $gifts
+#loop through gifts and declare what will be used to wrap
+foreach ($gift in $gifts) {
+  $wrapped_gift = "**" + $gift + "**"
+  #declare how long the gift is = how much wrapping paper required
+  $wrappingpaper = "*" * ($wrapped_gift.Length)
 
-$wrapped_gift = ""
-
-$wrapped_gift = "**" + $gift + "**"
-
-$wrappingpaper = "*" * ($wrapped_gift.Length)
-
-Write-Output $wrappingpaper
-Write-Output $wrapped_gift
-Write-Output $wrappingpaper
+  #wrap it all up in time for xmas
+  Write-Output $wrappingpaper
+  Write-Output $wrapped_gift
+  Write-Output $wrappingpaper
+  
+  # Output an empty line to make it pretty
+  Write-Output ""
+}
